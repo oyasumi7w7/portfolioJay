52ff697155bc7bebe7fedc5f870d4dd9aa6dcdb7
Alongkorn Thepwong <oyasumi.7w7@gmail.com>
2022-07-25 15:03:16 +0700

    Update

---
+++ src/Components/App/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from '../../assets/react.svg'
+import './App.css'
+import Navbar from '../Navbar/Navbar'
+const App = () => {
+
+  return (
+    <div>
+      <h1>Ja<span className="highlight">mmm</span>ing</h1>
+      <div className="App">
+        <Navbar />
+      </div>
+    </div>
+  )
+
+}
+
+export default App
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from "react"
+import './Navbar.css';
+
+const Navbar = (props) => {
+    return (
+        <nav>
+            <div>
+                <ui>
+                    <li><a>Experience</a></li>
+                    <li><a>Project</a></li>
+                    <li><a>Skill</a></li>
+                    <li><a>Contact</a></li>
+                    <li><a>Resume</a></li>
+                </ui>
+
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar 
