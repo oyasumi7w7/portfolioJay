701041ad7e969e0a52c57ae00c232311fc7e20eb
Alongkorn Thepwong <oyasumi.7w7@gmail.com>
2022-08-19 11:12:38 +0700

    update

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -9,20 +9,20 @@ const Contact = () => {
                 <div className='email-content'>
                     <div className='rows'>
                         <div className='cols'>
-                            <img src='../iconmonstr-email-10.svg' />
-                            <a href="mailto:jay.alongkorn.th@gmail.com"><p>Email : jay.alongkorn.th@gmail.com</p></a>
+                            <a href="mailto:jay.alongkorn.th@gmail.com"><img src='../iconmonstr-email-10.svg' />
+                            <p>Email : jay.alongkorn.th@gmail.com</p></a>
                         </div>
                         <div  className='cols'>
                             <img src='../Ic_phone_android_48px.svg' />
                             <p>Phone : 061-685-5982</p>
                         </div>
                         <div  className='cols'>
-                            <img src='../imgs/Octicons-mark-github.svg' />
-                            <a href="https://github.com/oyasumi7w7" target="_blank"><p>My GitHub</p></a>
+                            <a href="https://github.com/oyasumi7w7" target="_blank"><img src='../imgs/Octicons-mark-github.svg' />
+                            <p>My GitHub</p></a>
                         </div>
                         <div  className='cols'>
-                            <img src='../imgs/61109.png' />
-                            <a href="https://www.linkedin.com/in/alongkorn-thepwong-7a7221246/" target="_blank"><p>My LinkIn</p></a>
+                            <a href="https://www.linkedin.com/in/alongkorn-thepwong-7a7221246/" target="_blank"><img src='../imgs/61109.png' />
+                            <p>My LinkIn</p></a>
                         </div>
                     </div>
                 </div>
