dd188a7d3c47665fedd9d4cca54d5a76f3069aa6
Alongkorn Thepwong <oyasumi.7w7@gmail.com>
2022-11-25 12:58:49 +0700

    new

--- src/Components/Project/Project.jsx
+++ src/Components/Project/Project.jsx
@@ -18,6 +18,27 @@ const Project = () => {
                     nav
                     margin={8} >
                     <div className='card'>
+                        <h1>CM Busking</h1>
+                        <div className='flex-card'>
+                            <div className='pic'>
+                                <a href="https://chiangmai-busking-frontend.vercel.app/" target="_blank">
+                                    <img className="img" src={'../imgs/cm.busking.png'} /></a>
+                            </div>
+                            <div className="tool">
+                                <div className='col-img'>
+                                    <img src="../imgs/HTML5_logo_and_wordmark.svg" />
+                                    <img src="../imgs/React-icon.svg" />
+                                    <img src="../imgs/CSS3_logo_and_wordmark.svg" />
+                                    <img src="../imgs/MongoDB_Logo.svg" />
+                                </div>
+                                {/* <div className='github'>
+                                    <a href="https://github.com/oyasumi7w7/fit2bFrontEnd" target="_blank">
+                                        <img src="../imgs/Octicons-mark-github.svg" /></a>
+                                </div> */}
+                            </div>
+                        </div>
+                    </div>
+                    <div className='card'>
                         <h1>Jamming</h1>
                         <div className='flex-card'>
                             <div className='pic'>
@@ -52,8 +73,8 @@ const Project = () => {
                                     <img src="../imgs/Database-mysql.svg" />
                                 </div>
                                 <div className='github'>
-                                <a href="https://github.com/oyasumi7w7/monnymon" target="_blank">
-                                    <img src="../imgs/Octicons-mark-github.svg" /></a>
+                                    <a href="https://github.com/oyasumi7w7/monnymon" target="_blank">
+                                        <img src="../imgs/Octicons-mark-github.svg" /></a>
                                 </div>
                             </div>
                         </div>
@@ -62,8 +83,8 @@ const Project = () => {
                         <h1>Fit2b</h1>
                         <div className='flex-card'>
                             <div className='pic'>
-                            <a href="https://fit2b-front-end.vercel.app/" target="_blank">
-                                <img className="img" src={'../imgs/2bfit.png'} /></a>
+                                <a href="https://fit2b-front-end.vercel.app/" target="_blank">
+                                    <img className="img" src={'../imgs/2bfit.png'} /></a>
                             </div>
                             <div className="tool">
                                 <div className='col-img'>
@@ -83,6 +104,7 @@ const Project = () => {
                     <div><img className="img" src={'assets/img/img5.jpg'} /></div>
                     <div><img className="img" src={'assets/img/img6.jpg'} /></div>
                     <div><img className="img" src={'assets/img/img7.jpg'} /></div> */}
+
                 </OwlCarousel>
             </div>
         </div>
